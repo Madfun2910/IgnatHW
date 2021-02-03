168a3d2db68bb57245a9318a452249425656a9e0
Madfun2910 <Madfun2910@gmail.com>
2021-02-03 16:24:26 +0300

    homework 5

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,12 +1,31 @@
 import React from "react";
+import {PATH} from "./Routes";
+import {NavLink} from "react-router-dom";
+import s from "./Header.module.css"
 
 function Header() {
     return (
         <div>
-            // add NavLinks
-
+            <Navbar/>
         </div>
     );
 }
 
+function Navbar() {
+
+    return (
+        <div>
+            <NavLink className={s.likeLink} to={PATH.PRE_JUNIOR}>PreJunior</NavLink>
+            <NavLink className={s.likeLink} to={PATH.JUNIOR}>Junior</NavLink>
+            <NavLink className={s.likeLink} to={PATH.JUNIOR_PLUS}>JuniorPlus</NavLink>
+        </div>
+    );
+}
+
+
 export default Header;
+
+
+
+
+
