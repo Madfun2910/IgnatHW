660e53b1794a66086af68546c30ae2b32e45ccce
Madfun2910 <Madfun2910@gmail.com>
2021-02-03 14:52:25 +0300

    Updates

---
+++ precache-manifest.55534e13596de7f2381a8b7880798972.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03426aa573205e1c5bd1ff1597a613d0",
+    "url": "/IgnatHW/index.html"
+  },
+  {
+    "revision": "f8777da2af8043533962",
+    "url": "/IgnatHW/static/css/main.9a38c0f1.chunk.css"
+  },
+  {
+    "revision": "92faccda99ac8a3f68f1",
+    "url": "/IgnatHW/static/js/2.6f3c9da1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/IgnatHW/static/js/2.6f3c9da1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8777da2af8043533962",
+    "url": "/IgnatHW/static/js/main.19a3fc17.chunk.js"
+  },
+  {
+    "revision": "d80431a275551748f3a2",
+    "url": "/IgnatHW/static/js/runtime-main.76cb09e3.js"
+  }
+]);
